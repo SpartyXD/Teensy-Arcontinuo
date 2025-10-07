11b5ee687f2a613c5e71720e41eda728c1bf73d5
RadiosMark <dpgarces@uc.cl>
2025-10-07 12:29:28 -0300

    Update main.cpp

--- Codigo_nuevo_ESP32/src/main.cpp
+++ Codigo_nuevo_ESP32/src/main.cpp
@@ -5,6 +5,7 @@
 */
 
 #include <Arduino.h>
+<<<<<<< Updated upstream
 #include <midi.h>
 #include <data.h>
 #include <misc.h>
@@ -23,6 +24,30 @@ void setup(){
   initMIDI();
   Serial.begin(115200);
   initData(16, 17, 115200);
+=======
+#include <EEPROM.h>
+#include "Adafruit_TinyUSB.h"
+#include "MIDI.h"
+#include "utils.h"
+#include "pitchHandler.h"
+#include "gyro.h"
+
+Gyro gyro;
+Utils utils;
+PitchHandler pitchHandler;
+
+Adafruit_USBD_MIDI usb_midi;
+MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDI);
+
+// Arcontinuo configuration
+int serialNumber = 0;
+int gyroEnabled = 0;
+int gyroCounter = 0;
+
+void setup(){
+  MIDI.begin();
+  while (!TinyUSBDevice.mounted()) delay(1);
+>>>>>>> Stashed changes
 }
 
 
